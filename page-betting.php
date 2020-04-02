5ae8a1d9c25444bc6fae87dded131a2f9edbcd0d
Kevin Stradtman <stradtkt22@gmail.com>
2020-04-02 15:56:08 -0400

    Initial Commit

--- page-betting.php
+++ page-betting.php
@@ -5,10 +5,37 @@
 get_header();
 ?>
 
-<section>
+<section id="nfl-betting">
     <div class="container">
         <div class="row">
-            
+            <div class="col-12 col-md-10">
+                <div class="nfl-betting selection">
+                    <div class="title">
+                        <a href="<?php echo site_url('/betting'); ?>" class="betting-category-link"><h2 class="text-center text-white">NFL</h2></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-2"></div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-10">
+                <div class="mlb-betting selection">
+                    <div class="title">
+                        <a href="<?php echo site_url('/betting'); ?>" class="betting-category-link"><h2 class="text-center text-white">MLB</h2></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-2"></div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-10">
+                <div class="nba-betting selection">
+                    <div class="title">
+                        <a href="<?php echo site_url('/betting'); ?>" class="betting-category-link"><h2 class="text-center text-white">NBA</h2></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-2"></div>
         </div>
     </div>
 </section>
--- page-home.php
+++ page-home.php
@@ -12,7 +12,7 @@ get_header();
                 <div class="fantasy">
                     <div class="overlay d-flex flex-column">
                         <h1 class="text-center text-white mt-auto">Fantasy</h1>
-                        <a href="" disabled class="btn btn-danger btn-round mt-auto">Coming Soon</a>
+                        <a href="" disabled class="btn btn-danger mt-auto">Coming Soon</a>
                     </div>
                 </div>
             </div>
@@ -20,8 +20,39 @@ get_header();
                 <div class="betting">
                     <div class="overlay d-flex flex-column">
                         <h1 class="text-center text-white mt-auto">Betting</h1>
-                        <a href="" class="btn btn-danger btn-round mt-auto">Enter</a>
+                        <a href="<?php echo site_url('/betting'); ?>" class="btn btn-danger mt-auto">Enter</a>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="features">
+    <div class="container">
+        <div class="row mt-5 mb-5">
+            <div class="col-12 col-md-3">
+                <div class="card card-content feature-cards p-2">
+                    <p class="text-center"><i class="fas fa-calendar-plus"></i></p>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum repudiandae soluta, laudantium rerum eligendi!</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-3">
+                <div class="card card-content feature-cards p-2">
+                    <p class="text-center"><i class="fas fa-money-bill-alt"></i></p>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum repudiandae soluta, laudantium rerum eligendi!</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-3">
+                <div class="card card-content feature-cards p-2">
+                    <p class="text-center"><i class="fab fa-cc-stripe"></i><i class="fab fa-cc-paypal"></i></p>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum repudiandae soluta, laudantium rerum eligendi!</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-3">
+                <div class="card card-content feature-cards p-2">
+                    <p class="text-center"><i class="fas fa-newspaper"></i></p>
+                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum repudiandae soluta, laudantium rerum eligendi!</p>
                 </div>
             </div>
         </div>
