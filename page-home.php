ca7e405b53fa197eeacd556023bf0cc3a359fb40
Kevin Stradtman <stradtkt22@gmail.com>
2020-04-07 17:36:18 -0400

    Added some to home page and to css

--- page-home.php
+++ page-home.php
@@ -9,10 +9,8 @@ get_header();
     <div class="container">
             <div class="col-12">
                 <div class="betting">
-                    <div class="overlay">
-                        <h1 class="text-center text-white">Drego Sports Betting Guide</h1>
-                        <p class="lead text-center text-white">Everyone Has Their Own Bets</p>
-                    </div>
+                    <h1 class="text-center text-white">Drego Sports Betting Guide</h1>
+                    <p class="lead text-center text-white">Everyone Has Their Own Bets</p>
                 </div>
             </div>
         </div>
@@ -23,33 +21,26 @@ get_header();
     <div class="container">
         <div class="row mt-5 mb-5">
             <div class="col-12 col-md-3">
-                <div class="card card-content feature-cards p-2">
                     <p class="text-center"><i class="fas fa-calendar-plus"></i></p>
-                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum repudiandae soluta, laudantium rerum eligendi!</p>
-                </div>
+                    <p class="text-center">Updated daily.  The games will be updated daily and will be sent out for the final results right before the games start at the beginning of the week.</p>
             </div>
             <div class="col-12 col-md-3">
-                <div class="card card-content feature-cards p-2">
                     <p class="text-center"><i class="fas fa-money-bill-alt"></i></p>
-                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum repudiandae soluta, laudantium rerum eligendi!</p>
-                </div>
+                    <p class="text-center">Have more accurate predictions than competing sites.  Our results and numbers have cme from thorough analysis and trials before we actually give you the correct numbers.</p>
             </div>
             <div class="col-12 col-md-3">
-                <div class="card card-content feature-cards p-2">
                     <p class="text-center"><i class="fab fa-cc-stripe"></i><i class="fab fa-cc-paypal"></i></p>
-                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum repudiandae soluta, laudantium rerum eligendi!</p>
-                </div>
+                    <p class="text-center">We go through stripe and paypal.  Your connection will be secure upon checkout.  You have no worries of unwanted charges from us or someone else.</p>
             </div>
             <div class="col-12 col-md-3">
-                <div class="card card-content feature-cards p-2">
                     <p class="text-center"><i class="fas fa-newspaper"></i></p>
-                    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum cum repudiandae soluta, laudantium rerum eligendi!</p>
-                </div>
+                    <p class="text-center">Sign up for the newsletter.  This newsletter will show you when we are having deals with our membership, and will provide more information about data from our site.</p>
             </div>
         </div>
     </div>
 </section>
 
+<hr class="hr">
 
 <section id="plans">
     <div class="container">
@@ -115,6 +106,66 @@ get_header();
     </div>
 </section>
 
+<hr class="hr">
+
+<section id="testimonials">
+    <div class="container">
+        <div class="row mt-5 mb-5">
+            <div class="col-12 col-md-3">
+                <blockquote>
+                    <h4>Kevin Stradtman</h4>
+                    <p>"This site has made me a better gambler.  Thanks to Drego Sports I can bring in additional income for me and my family!"</p>
+                </blockquote>
+                <p>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </p>
+            </div>
+            <div class="col-12 col-md-3">
+                <blockquote>
+                    <h4>Dominic Lattarulo</h4>
+                    <p>"Thanks Drego Sports so much for your constant updates on the NFL games.  You should get into MLB and NBA!"</p>
+                </blockquote>
+                <p>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </p>
+            </div>
+            <div class="col-12 col-md-3">
+                <blockquote>
+                    <h4>Adam Mair</h4>
+                    <p>"I couldn't thank Drego Sports enough for what they have done for my betting career.  They have brought me out of the slumps."</p>
+                </blockquote>
+                <p>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </p>
+            </div>
+            <div class="col-12 col-md-3">
+                <blockquote>
+                    <h4>Mary Shwimmer</h4>
+                    <p>"This is definitely the site to go off of for betting advice for the NFL.  You guys are awesome.  Don't quit doing what you are doing."</p>
+                </blockquote>
+                <p>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php
 get_footer();
